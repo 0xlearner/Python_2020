99988eb3c55e84f94a2b9385433a84f5a5809e57
iamtauqeer <iamtauqeerabbas@gmail.com>
2020-02-18 23:33:38 +0500

    first_app

---
+++ hello_world/program.py
@@ -0,0 +1,7 @@
+print('----------------------------------')
+print('        HELLO_PYTHONIC_WORLD')
+print('----------------------------------')
+
+user_text = input('What is your name? ')
+print('')
+print('Nice to meet you, ' + user_text)
