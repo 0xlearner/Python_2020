1b5216d2241fbf4bfb637f110072a17aa59c8d5a
xlearner <iamtauqeerabbas@gmail.com>
2020-02-24 17:16:23 +0500

    Letter_Count_App

--- hello_world/program.py
+++
@@ -1,7 +0,0 @@
-print('----------------------------------')
-print('        HELLO_PYTHONIC_WORLD')
-print('----------------------------------')
-
-user_text = input('What is your name? ')
-print('')
-print('Nice to meet you, ' + user_text)
