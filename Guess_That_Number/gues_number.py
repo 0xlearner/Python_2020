9b3b9de3f9292a2763d0cd107e8f75efb22616a2
xlearner <iamtauqeerabbas@gmail.com>
2020-03-03 09:50:10 +0500

    GUESS_THAT_NUMBER_GAME

--- Guess_That_Number/gues_number.py
+++ Guess_That_Number/gues_number.py
@@ -6,13 +6,14 @@ print('-' * 30 + '\n')
 
 the_number = random.randint(1, 100)
 guess_number = False
+player_name = input('What is your name? ')
 
 while guess_number != the_number:
     guess_number = int(input('Guess a number between 1 to 100: '))
 
     if guess_number < the_number:
-        print('Too Low')
+        print(f'Sorry,{player_name.title()} your guess of {guess_number} is too low.')
     elif guess_number > the_number:
-        print('Too High')
+        print(f'Sorry,{player_name.title()} your guess of {guess_number} is too high.')
     else:
-        print('You Win!')
+        print(f'Congrats {player_name.title()}! you won!!')
