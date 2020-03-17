2f74a86d66bfb0f320d844ceb485c1d39b0ea185
xlearner <iamtauqeerabbas@gmail.com>
2020-03-17 23:02:13 +0500

    Guess_my_Number

---
+++ Guess_My_Number/guess_num.py
@@ -0,0 +1,22 @@
+import random
+
+print('Guess my Number')
+
+user = input('\nHello!What is your name > ')
+
+print(f'Well!{user} i am thinking of a number between 1 to 20.')
+number = random.randint(1,20)
+
+for guess_num in range(5):
+    guess = int(input('\nTake a Guess >'))
+
+    if guess< number:
+        print('Your Guess is too Low')
+    elif guess > number:
+        print('Your Guess is too High')
+    else:
+        break
+if guess == number:
+    print(f'Good job! {user} you guessed my number in {guess_num+1} guesses!')
+else:
+    print(f'\nGame Over...The number I was thinking of was {number}.')
